08dba91b6c5ea82472aad9ae12bf21301402c2bd
Sivaram Kannan <sivaramsk@gmail.com>
2019-06-07 17:00:45 +0530

    Convert ansible deployment to bash scripts as ansible does not support windows as control machine.

---
+++ setup/install_components.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function install_prereq_packages {
+	apt-get update && apt-get dist-upgrade && \
+		apt-get install -y apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common \
+		bash-completion
+}
+
+
+function install_docker {
+	sudo apt-get install -y docker.io
+	sudo bash -c 'cat << EOF > /etc/docker/daemon.json
+	{
+	   "exec-opts": ["native.cgroupdriver=systemd"]
+        }
+	EOF'   
+
+}
+
+function add_vagrant_user_to_docker_group {
+}
+
+function disable_swap {
+}
+
+function install_k8s {
+	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+	   
+	sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/kubernetes.list
+	deb http://apt.kubernetes.io/ kubernetes-xenial main
+	EOF'
+	   
+	   
+	sudo apt update
+	   
+	sudo apt install -y kubelet kubeadm kubectl
+}
+
+function initialize_k8s_kubeadm {
+}
+
+function add_node_iP_param_to_kubeadm {
+}
+
+
