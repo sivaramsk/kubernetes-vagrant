86ed3aa9e128d63d9a39403b90c0ce6751d95ea6
Sivaram Kannan <sivaramsk@gmail.com>
2019-06-09 21:20:30 +0530

    Incomplete file. Committed as I need to change laptop.

--- setup/install_components.sh
+++ setup/install_components.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source versions
+
+#Configurable Variables
+K8S_UBUNTU_PACKAGE_VERSION=1.14.2-00
 
 function install_prereq_packages {
-	apt-get update && apt-get dist-upgrade && \
+	apt-get update && apt-get -y upgrade && \
 		apt-get install -y apt-transport-https \
 		ca-certificates \
 		curl \
@@ -18,33 +20,38 @@ function install_docker {
 	{
 	   "exec-opts": ["native.cgroupdriver=systemd"]
         }
-	EOF'   
+	EOF'
 
 }
 
 function add_vagrant_user_to_docker_group {
+	usermod -aG docker vagrant 
 }
 
 function disable_swap {
+	swapoff -a
 }
 
 function install_k8s {
 	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-	   
+
 	sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 	deb http://apt.kubernetes.io/ kubernetes-xenial main
 	EOF'
-	   
-	   
+
+
 	sudo apt update -y
-	   
+
 	sudo apt install -y kubelet=${K8S_UBUNTU_PACKAGE_VERSION} kubeadm=${K8S_UBUNTU_PACKAGE_VERSION} kubectl=${K8S_UBUNTU_PACKAGE_VERSION}
 }
 
-function initialize_k8s_kubeadm {
+function main {
+	install_prereq_packages
+	install_docker
+	add_vagrant_user_to_docker_group
+	disable_swap
+	install_k8s
+
 }
 
-function add_node_iP_param_to_kubeadm {
-}
-
-
+main 
